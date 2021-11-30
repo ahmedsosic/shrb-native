2b1a2edb4d0e782649645671eded5e8e0642da91
ismailsosic <ismailsosic@gmail.com>
2021-11-30 15:57:34 +0100

    add HomeScreen and some style

--- shrbApp/Screens/HomeScreen.js
+++ shrbApp/Screens/HomeScreen.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, View, Text, ScrollView } from 'react-native';
+import Card from './components/Card';
 
-export default function App() {
+
+
+const HomeScreen = () => {
+
+    const [cards, setCards] = useState([
+    {date_from: '12-12-2021', date_to: '13-12-2021', description: 'odmor', key: 1},
+    {date_from: '22-10-2021', date_to: '23-10-2021', description: 'bolovanje', key: 2},
+    {date_from: '12-12-2021', date_to: '13-12-2021', description: 'odmor', key: 3},
+    {date_from: '22-10-2021', date_to: '23-10-2021', description: 'bolovanje', key: 4},
+    {date_from: '12-12-2021', date_to: '13-12-2021', description: 'odmor', key: 5},
+    {date_from: '22-10-2021', date_to: '23-10-2021', description: 'bolovanje', key: 6},
+    {date_from: '12-12-2021', date_to: '13-12-2021', description: 'odjhkjh hkjhkjh jhkjhkj jkhmor', key: 7},
+    {date_from: '22-10-2021', date_to: '23-10-2021', description: 'bolovanje', key: 8},
+    {date_from: '12-12-2021', date_to: '13-12-2021', description: 'odmor', key: 9},
+    {date_from: '22-10-2021', date_to: '23-10-2021', description: 'bolovanje', key: 10},
+  ])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <View style={styles.cardList}>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                data={cards}
+                renderItem={({item}) => (
+                    <Card card={item} />
+                )} 
+            />
+        </View>
     </View>
   );
 }
@@ -18,4 +41,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  cardList: {
+    marginTop: 30,
+    flex: 1,
+    overflow: 'hidden',
+
+  },
 });
+
+export default HomeScreen
