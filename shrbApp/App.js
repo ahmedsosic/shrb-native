d605cb78fb42b9d8e0ee37f3f7ad7ef2ce910e83
Ahmed Sosic <ahmedsosic@gmail.com>
2021-11-30 18:59:58 +0100

    add cardscreen and styling

--- shrbApp/App.js
+++ shrbApp/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './Screens/HomeScreen';
 import LoginScreen from './Screens/LoginScreen';
+import CardScreen from './Screens/CardScreen';
 
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <LoginScreen />
+      <CardScreen />
     </View>
   );
 }
